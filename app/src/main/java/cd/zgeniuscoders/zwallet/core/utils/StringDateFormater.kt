5c67e5c77f38d11e1e96d595964d5b0f00700c88
zgeniusecoders <zgeniusecoders@gmail.com>
2025-06-28 18:26:26 +0100

    feat: ajout  de la methode  pour  convertir  une chaime  de caratere en  format LocalDateTime

--- app/src/main/java/cd/zgeniuscoders/zwallet/core/utils/StringDateFormater.kt
+++ app/src/main/java/cd/zgeniuscoders/zwallet/core/utils/StringDateFormater.kt
@@ -1,5 +1,6 @@
 package cd.zgeniuscoders.zwallet.core.utils
 
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -8,4 +9,10 @@ fun String.formatDate(): String? {
     var outputFormater = DateTimeFormatter.ofPattern("d/M/yyyy")
     var dateTime = LocalDateTime.parse(this, inputFormater)
     return dateTime.format(outputFormater)
+}
+
+fun String.toLocalDate(): LocalDateTime {
+    var inputFormater = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+    var localDateTime = LocalDateTime.parse(this, inputFormater)
+    return localDateTime
 }
