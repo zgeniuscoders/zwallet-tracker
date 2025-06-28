e03fbafed05b5766464ffb98ee502331886e7596
zgeniusecoders <zgeniusecoders@gmail.com>
2025-06-28 06:54:23 +0100

    feat: création modele utilisateur

---
+++ app/src/main/java/cd/zgeniuscoders/zwallet/auth/domains/models/User.kt
@@ -0,0 +1,8 @@
+package cd.zgeniuscoders.zwallet.auth.domains.models
+
+data class User(
+    var id: String = "",
+    var email: String = "",
+    var username: String = "",
+    var photoProfile: String = ""
+)
