a33f63b552371e8e7fece3837f1e2d7d4923ad1d
zgeniusecoders <zgeniusecoders@gmail.com>
2025-06-28 12:00:28 +0100

    feat: ajout de l'attribut user id

--- app/src/main/java/cd/zgeniuscoders/zwallet/expense/domain/models/Expense.kt
+++ app/src/main/java/cd/zgeniuscoders/zwallet/expense/domain/models/Expense.kt
@@ -5,6 +5,7 @@ import java.time.LocalDateTime
 
 data class Expense(
     val id: String = "",
+    var userId: String = "",
     val amount: Double,
     val description: String,
     val observation: String = "",
