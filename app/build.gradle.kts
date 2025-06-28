6afdda6f1c742a9043c183214e5e092c74a19b07
zgeniusecoders <zgeniusecoders@gmail.com>
2025-06-28 07:59:24 +0100

    chore: installation du librarie kotlinSerialiation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
+    kotlin("plugin.serialization") version "2.0.10"
 }
 
 android {
