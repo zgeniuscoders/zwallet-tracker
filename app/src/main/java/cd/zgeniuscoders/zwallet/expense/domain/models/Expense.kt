85522a73cd9ec03446d5cf5c4dc23483f8084db5
zgeniusecoders <zgeniusecoders@gmail.com>
2025-06-28 15:12:26 +0100

    refactor: modification type date en String

--- app/src/main/java/cd/zgeniuscoders/zwallet/expense/domain/models/Expense.kt
+++ app/src/main/java/cd/zgeniuscoders/zwallet/expense/domain/models/Expense.kt
@@ -6,10 +6,10 @@ import java.time.LocalDateTime
 data class Expense(
     val id: String = "",
     var userId: String = "",
-    val amount: Double,
-    val description: String,
+    val amount: Double = 0.0,
+    val description: String = "",
     val observation: String = "",
-    val category: ExpenseCategory,
-    val date: LocalDateTime = LocalDateTime.now()
+    val category: ExpenseCategory = ExpenseCategory.NORMAL,
+    val date: String = ""
 )
 
