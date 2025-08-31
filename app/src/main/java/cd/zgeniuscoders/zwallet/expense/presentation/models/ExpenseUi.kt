4386350202b5b804042ab83e8d4822f1e70872a7
zgeniusecoders <zgeniusecoders@gmail.com>
2025-08-31 11:34:43 +0100

    feat:creation du modele ExpenseUi.kt

---
+++ app/src/main/java/cd/zgeniuscoders/zwallet/expense/presentation/models/ExpenseUi.kt
@@ -0,0 +1,13 @@
+package cd.zgeniuscoders.zwallet.expense.presentation.models
+
+import androidx.compose.ui.graphics.Color
+
+data class ExpenseUi(
+    var description: String,
+    var observation: String,
+    var date: String,
+    var amountFormat: String,
+    var amount: Double,
+    var category: String,
+    var color: Color
+)
