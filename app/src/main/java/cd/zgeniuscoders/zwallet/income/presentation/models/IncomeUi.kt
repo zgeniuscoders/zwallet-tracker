e73cb7786a4369d6a9730df9151b035a70fcf879
zgeniusecoders <zgeniusecoders@gmail.com>
2025-08-31 11:53:56 +0100

    feat: création du modele IncomeUi.kt

---
+++ app/src/main/java/cd/zgeniuscoders/zwallet/income/presentation/models/IncomeUi.kt
@@ -0,0 +1,8 @@
+package cd.zgeniuscoders.zwallet.income.presentation.models
+
+data class IncomeUi(
+    val description:String,
+    val amount:Double,
+    val amountFormat:String,
+    val date:String
+)
