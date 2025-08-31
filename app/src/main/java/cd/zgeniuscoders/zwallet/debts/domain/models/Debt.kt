2a622b4dd74ba180993f6e2f249d69faa0f83eff
zgeniusecoders <zgeniusecoders@gmail.com>
2025-08-31 12:50:22 +0100

    feat: création du Presentation -> DebtMapper.kt

--- app/src/main/java/cd/zgeniuscoders/zwallet/debts/domain/models/Debt.kt
+++ app/src/main/java/cd/zgeniuscoders/zwallet/debts/domain/models/Debt.kt
@@ -3,10 +3,10 @@ package cd.zgeniuscoders.zwallet.debts.domain.models
 data class Debt(
     val id: String = "",
     val userId: String = "",
-    val amount: Double = 0.0,
-    val description: String = "",
-    val creditorName: String = "",
-    val dueDate: String = "",
-    val isOwedByMe: Boolean = false, // true si je dois, false si on me doit
+    val amount: Double,
+    val description: String,
+    val creditorName: String,
+    val dueDate: String,
+    val isOwedByMe: Boolean = false,
     val isPaid: Boolean = false
 )
---
+++ app/src/main/java/cd/zgeniuscoders/zwallet/debts/presentation/mappers/DebtMapper.kt
@@ -0,0 +1,24 @@
+package cd.zgeniuscoders.zwallet.debts.presentation.mappers
+
+import cd.zgeniuscoders.zwallet.core.utils.formatDate
+import cd.zgeniuscoders.zwallet.core.utils.toLocalDate
+import cd.zgeniuscoders.zwallet.debts.domain.models.Debt
+import cd.zgeniuscoders.zwallet.debts.presentation.models.DebtUi
+import java.time.LocalDateTime
+
+fun List<Debt>.toDebtUiModel(): List<DebtUi> {
+    return this.map {
+
+        DebtUi(
+            id = it.id,
+            week = it.dueDate.toLocalDate().isBefore(LocalDateTime.now().plusDays(7)),
+            amount = it.amount,
+            description = it.description,
+            isOwedByMe = it.isOwedByMe,
+            date = it.dueDate.formatDate() ?: "",
+            isOverdue = it.dueDate.toLocalDate().isBefore(LocalDateTime.now()),
+            creditor = it.creditorName,
+            amountFormat = "${it.amount} CDF",
+        )
+    }
+}
