cbc3412d17738b455995aca537c547bc85352220
zgeniusecoders <zgeniusecoders@gmail.com>
2025-08-31 12:50:03 +0100

    feat: création du DebtUi.kt

---
+++ app/src/main/java/cd/zgeniuscoders/zwallet/debts/presentation/models/DebtUi.kt
@@ -0,0 +1,15 @@
+package cd.zgeniuscoders.zwallet.debts.presentation.models
+
+import java.time.LocalDateTime
+
+data class DebtUi(
+    val id: String,
+    val isOverdue: Boolean,
+    val week: Boolean,
+    val isOwedByMe: Boolean,
+    val date: String,
+    val creditor: String,
+    val description: String,
+    val amount: Double,
+    val amountFormat: String,
+)
